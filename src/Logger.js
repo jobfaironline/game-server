0bc7277ebbe52ec207cf6ad419d26aec531809af
Tien T. Truong <tientt1938@gmail.com>
2022-04-06 17:02:49 +0700

    add log folder

--- src/Logger.js
+++ src/Logger.js
@@ -1,4 +1,5 @@
 import pkg from "simple-node-logger";
+import fs from "fs";
 const {createSimpleLogger} = pkg;
 
 export default class Logger{
@@ -7,8 +8,11 @@ export default class Logger{
     const date = ("0" + dateObj.getDate()).slice(-2);
     const month = ("0" + (dateObj.getMonth() + 1)).slice(-2);
     const year = dateObj.getFullYear();
+    if (!fs.existsSync("log")){
+      fs.mkdirSync("log");
+    }
 
-    this.logger = createSimpleLogger(`${year}-${month}-${date}-server.log`);
+    this.logger = createSimpleLogger(`log/${year}-${month}-${date}-server.log`);
   }
   info(...obj){
     this.logger.info(obj);
