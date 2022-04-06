1de31daf5c703d236a5f50af5227055ad140dc55
Tien T. Truong <tientt1938@gmail.com>
2022-04-06 17:11:30 +0700

    add env variable for stun server

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ import {iceServers as defautlIceServers} from "@geckos.io/server";
 
 dotenv.config();
 
-
+const logger = new Logger();
 let iceServers = defautlIceServers;
 if (process.env.STUN_URL !== undefined) {
-  iceServers = [{urls: process.env.STUN_URL, username: process.env.STUN_USERNAME, credential: process.env.STUN_CRED}];
+  const stunInfo = {urls: process.env.STUN_URL, username: process.env.STUN_USERNAME, credential: process.env.STUN_CRED}
+  logger.info("STUN_SERVER", stunInfo)
+
+  iceServers = [stunInfo];
 }
-const logger = new Logger();
 const server = new Server(3001, logger, iceServers);
 server.start();
 
